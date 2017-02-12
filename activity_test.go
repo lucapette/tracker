d4f0bfdc884b44e6f8bda220a82658b28c62b0ce
lucapette <lucapette@gmail.com>
2017-02-12 23:59:00 +0100

    Ehm I forgot to run the tests :)

--- activity_test.go
+++ activity_test.go
@@ -10,12 +10,11 @@ func TestNewActivity(t *testing.T) {
 		name     string
 		expected Activity
 	}{
-		{name: "iTerm2", expected: Activity{Name: "iTerm2", Category: Development}},
-		{name: "http://localhost:3000", expected: Activity{Name: "localhost", Category: Development}},
-		{name: "https://twitter.com", expected: Activity{Name: "twitter.com", Category: Social}},
-		{name: "http://whatever.dev", expected: Activity{Name: "whatever.dev", Category: Development}},
-		{name: "https://twitter.com?with=parsms&it=works&anyway=right", expected: Activity{Name: "twitter.com", Category: Social}},
-		{name: "airmail", expected: Activity{Name: "airmail", Category: Communication}},
+		{name: "iTerm2", expected: Activity{Name: "iTerm2", Category: Categories["Development"]}},
+		{name: "http://localhost:3000", expected: Activity{Name: "localhost", Category: Categories["Development"]}},
+		{name: "https://twitter.com", expected: Activity{Name: "twitter.com", Category: Categories["Social"]}},
+		{name: "https://twitter.com?with=parsms&it=works&anyway=right", expected: Activity{Name: "twitter.com", Category: Categories["Social"]}},
+		{name: "airmail", expected: Activity{Name: "airmail", Category: Categories["Communication"]}},
 	}
 
 	for _, test := range tests {
